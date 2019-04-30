f89aa1c48f7fbaba07ba9a0742886f040dc03d55
Jonatas-Felipe <46595834+Jonatas-Felipe@users.noreply.github.com>
2019-04-30 17:33:16 -0300

    Add files via upload

--- routes.js
+++ routes.js
@@ -9,6 +9,7 @@ const FileController = require('./controllers/FileController');
 
 routes.post("/boxes", BoxController.store)
 routes.get("/boxes/:id", BoxController.show)
+routes.get("/all_boxes", BoxController.all)
 routes.post("/boxes/:id/files", multer(multerConfig).single('file'), FileController.store)
 
 module.exports = routes;
